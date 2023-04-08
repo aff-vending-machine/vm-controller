663db86017eb08e709b11a33345d3dee56fe3208
Tanawat Hongthai <ztrixack.th@gmail.com>
2023-04-08 14:28:17 +0700

    add shortcut paid

--- internal/layer/flow/jetts/stages/payment/on_key_pressed.go
+++ internal/layer/flow/jetts/stages/payment/on_key_pressed.go
@@ -6,13 +6,17 @@ import (
 )
 
 func (s *stageImpl) OnKeyPressed(c *flow.Ctx, key hardware.Key) error {
-	switch key.ToString() {
-	case hardware.StarKey:
+	switch key.Type() {
+	case hardware.STAR:
 		c.PaymentChannel = nil
 		if s.ticker != nil {
 			s.ticker.Stop()
 		}
 		c.ChangeStage <- "payment_channel"
+
+	case hardware.SHARP:
+		s.updateTestTransaction(c)
+		c.ChangeStage <- "receive"
 	}
 
 	return nil
--- internal/layer/flow/jetts/stages/payment/transaction.go
+++ internal/layer/flow/jetts/stages/payment/transaction.go
@@ -46,6 +46,26 @@ func (s *stageImpl) updatePaidTransaction(c *flow.Ctx) error {
 	return nil
 }
 
+func (s *stageImpl) updateTestTransaction(c *flow.Ctx) error {
+	filter := []string{fmt.Sprintf("merchant_order_id:=:%s", c.Data.MerchantOrderID)}
+	data := map[string]interface{}{
+		"order_status":      enum.ORDER_STATUS_PAID,
+		"confirmed_paid_by": "test",
+		"confirmed_paid_at": time.Now(),
+		"is_error":          false,
+		"error":             "(test)",
+		"error_at":          time.Now(),
+	}
+
+	_, errx := s.transactionRepo.UpdateMany(c.UserCtx, filter, data)
+	if errx != nil {
+		log.Error().Err(errx).Str("order_id", c.Data.MerchantOrderID).Interface("data", data).Msg("TRANSACTION: unable to update transaction")
+		return errx
+	}
+
+	return nil
+}
+
 func (s *stageImpl) updateCancelTransaction(c *flow.Ctx) error {
 	filter := []string{fmt.Sprintf("merchant_order_id:=:%s", c.Data.MerchantOrderID)}
 	data := map[string]interface{}{
