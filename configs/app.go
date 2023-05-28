bab07f5cb40d39c799f80b93368d68eba7ca2888
Tanawat Hongthai <ztrixack.th@gmail.com>
2023-05-29 00:45:26 +0700

    🔥 chore(app.go): remove CenterConfig struct and its fields ✨ feat(fiber.go): change default AppName to 'vending-machine-controller' 🔥 chore(sqlite.go): remove Enable field from SQLiteConfig struct

--- configs/app.go
+++ configs/app.go
@@ -4,15 +4,7 @@ type AppConfig struct {
 	ENV      string        `default:"production" mapstructure:"ENV"`
 	LogLevel int           `default:"0" mapstructure:"LOG_LEVEL"`
 	Preload  bool          `default:"false" mapstructure:"PRELOAD"`
-	Jaeger   bool          `default:"false" mapstructure:"JAEGER"`
-	Center   CenterConfig  `mapstructure:"CENTER"`
 	Machine  MachineConfig `mapstructure:"MACHINE"`
-}
-
-type CenterConfig struct {
-	RPCQueue         string `default:"CT-APP-RPC" mapstructure:"RPC_QUEUE"`
-	TopicQueue       string `default:"CT-APP-TOPIC" mapstructure:"TOPIC_QUEUE"`
-	RoutingKeyHeader string `default:"routing-key" mapstructure:"ROUTING_KEY_HEADER"`
 }
 
 type MachineConfig struct {
--- configs/sqlite.go
+++ configs/sqlite.go
@@ -1,7 +1,6 @@
 package configs
 
 type SQLiteConfig struct {
-	Enable   bool   `default:"false" mapstructure:"ENABLE"`
 	LogLevel int    `default:"1" mapstructure:"LOG_LEVEL"`
 	Database string `default:"db/default.sqlite" mapstructure:"DATABASE"`
 }
